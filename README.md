41251c3e2a61b19f182277b765083a83787a9243
beiyuouo <beiyu233@gmail.com>
2020-08-09 12:20:36 +0800

    ver 0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Auto-Guichu v0.1
+2020.8.9
+base func
---
+++ utils/options.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @python: 3.6
+
+import os
+import argparse
+
+
+def args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video', type=str, default='./data')
+    parser.add_argument('--bgm', type=str, default='bgm.mp3')
+    parser.add_argument('--log', type=str, default='./log')
+    parser.add_argument('--output', type=str, default='./result')
+    parser.add_argument('--mini_seg', type=float, default=0.01)
+    parser.add_argument('--mini_eps', type=float, default=0.1)
+    parser.add_argument('--bgm_coefficient', type=float, default=2)
+    parser.add_argument('--video_coefficient', type=float, default=3)
+    parser.add_argument('--bias', type=float, default=0.1)
+    parser.add_argument('--opt', type=str, default='normal')
+    args = parser.parse_args()
+    return args
